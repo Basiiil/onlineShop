5575b6d3348565ba56de9969fb350e2b7edd38cf
Basiiil <basil.pros23@gmail.com>
2022-10-16 23:19:51 +0330

    feat eslint in project

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    // "plugin:prettier/recommended",
+  ],
+  overrides: [
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
+    semi: ['error', 'always'],
+    quotes: ['warn', 'single'],
+    curly: [2, 'all']
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    },
+   }
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App () {
+  return (
+    <div className="">
+      hii
+    </div>
+  );
+}
+
+export default App;
